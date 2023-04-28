d52722d0427284bd2c2e5e6ae019219afefd9e4a
boluwa111 <giwasamuel6@gmail.com>
2023-04-27 21:53:13 -0700

    ALX assignmnet

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # This script causes the server to respond with a message containing "You got me!"
 
-curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me --output /dev/null
+curl -s -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me -o /dev/null -w "You got me!\n" | tr -d '\n'
