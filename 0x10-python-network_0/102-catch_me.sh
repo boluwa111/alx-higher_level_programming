43d7e8e3f2331645db1ccc36a7a48455b4580ec8
boluwa111 <giwasamuel6@gmail.com>
2023-04-27 21:49:19 -0700

    ALX assignmnet

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,1 +1,4 @@
-404: Not Found
+#!/bin/bash
+# This script causes the server to respond with a message containing "You got me!"
+
+curl -s -X PUT -L -d "user_id=98" 0.0.0.0:5000/catch_me --output /dev/null
